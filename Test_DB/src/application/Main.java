3576115a8d0e650fd5fc4604ec9e2cc5ec55d909
Chiurito <60774362+Chiurito@users.noreply.github.com>
2023-02-24 22:41:57 +0100

    .

--- Test_DB/src/application/Main.java
+++ Test_DB/src/application/Main.java
@@ -6,40 +6,46 @@ import java.sql.SQLException;
 
 public class Main {
 
-	static String driverName = "com.mysql.cj.jdbc.Driver";
+	private static final String driverName = "com.mysql.cj.jdbc.Driver";
 	static String DB_URL = "jdbc:mysql://localhost:3306/mydb";
 	static String user = "rocco";
 	static String password = "password";
+	static Connection connection = null;
 
 	public static void main(String[] args) {
 		System.out.println("-------- MySQL JDBC Connection Demo ------------");
 		System.out.println(" ");
+
 		try
 		{
-			Class.forName(driverName);
-		} 
-		catch (ClassNotFoundException e) {
+			Class.forName(getDriverName());
+
+		} catch (ClassNotFoundException e) {
 			System.out.println("MySQL JDBC Driver not found!!");
-			return;
 		}
+
 		System.out.println("MySQL JDBC Driver Registered!");
-		Connection connection = null;
+
 		try {
 			connection = DriverManager.getConnection(DB_URL, user, password);
 			System.out.println("SQL Connection to database established...");
 
-
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console...");
+
 		} finally {
 			try
 			{
 				if(connection != null)
 					connection.close();
 				System.out.println("Connection closed...");
+
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	public static String getDriverName() {
+		return driverName;
 	}
 }
