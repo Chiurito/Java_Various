2145a7cbf36a38f9f70e36cf586e1e06bb1c5640
Chiurito <60774362+Chiurito@users.noreply.github.com>
2023-02-26 20:09:31 +0100

    .

---
+++ JavaFX_Test/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Main window");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
